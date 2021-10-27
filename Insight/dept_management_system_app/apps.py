3a0905f313b15294bc56321e729724602a714dc2
sachin2496 <sachin.kumar.cse20@itbhu.ac.in>
2021-10-27 13:46:49 +0530

    final commit

---
+++ Insight/dept_management_system_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class DeptManagementSystemAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'dept_management_system_app'
